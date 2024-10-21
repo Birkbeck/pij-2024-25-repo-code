340de54936456029afc186151646dd983de2daab
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2024-10-21 12:43:27 +0100

    Day 7, Dog update with explicit constructor and docs

--- src/Day7/pij/day7/animals/Dog.java
+++ src/Day7/pij/day7/animals/Dog.java
@@ -1,9 +1,24 @@
 package pij.day7.animals;
 
+/**
+ * A Dog is a LandAnimal that walks and that makes a "Woof!" sound.
+ */
 public class Dog extends LandAnimal {
 
-    // no explicit constructor -- implicitly calls super()
+    // final because we are not allowing for renaming;
+    // with the 'final' modifier, the compiler can check this for ys
+    public final String name;
 
+    /**
+     * Initialises the new Dog with the given name.
+     *
+     * @param name the Dog's name
+     */
+    public Dog(String name) {
+        // implicitly calls super(); if we wanted to call a different
+        // superclass constructor, we would have to do this explicitly here
+        this.name = name;
+    }
     @Override
     public void move(int metres) {
         System.out.println("Walking " + metres + "...");
