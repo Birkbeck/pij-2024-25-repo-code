2ba391d6bd6297f9d4bfb588053173d42e267c79
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2024-11-06 12:57:55 +0000

    Day 11, example class with comment how to generate a test class for it

---
+++ src/Day11/main/pij/day11/bob/Bob.java
@@ -0,0 +1,17 @@
+package pij.day11.bob;
+
+/**
+ * Example class to demonstrate how to generate tests for a given class
+ * via keyboard shortcut in IntelliJ: Use Alt-Insert on a PC (Windows/Linux)
+ * or Cmd-N on a Mac. (Alternative: right mouse button for context menu, then
+ * select "Generate".)
+ *
+ * Then click on "Test...". Then confirm for JUnit 5. A test class called
+ * BobTest will be generated for you in the same package, in the folder
+ * that you have marked for as "Test Sources Root" in your project.
+ * (If you have not yet marked a folder as "Test Sources Root", it is a
+ * good idea to do so, so that your JUnit tests are separate from your
+ * production code.)
+ */
+public class Bob {
+}
