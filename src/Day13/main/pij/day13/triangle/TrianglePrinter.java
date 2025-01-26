77d20a081da7d48aa0f89710b5b186e223cf664f
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2025-01-26 12:35:44 +0000

    Day 13 slightly extended TringlePrinter code

--- src/Day13/main/pij/day13/triangle/TrianglePrinter.java
+++ src/Day13/main/pij/day13/triangle/TrianglePrinter.java
@@ -8,6 +8,7 @@ public class TrianglePrinter {
     /**
      * Prints a Triangle of length sideLength on the screen.
      * Does nothing if sideLength is zero or negative.
+     * Uses recursion.
      *
      * @param sideLength  the length of the triangle to print
      */
@@ -23,10 +24,30 @@ public class TrianglePrinter {
     }
 
     /**
+     * Prints a Triangle of length sideLength on the screen.
+     * Does nothing if sideLength is zero or negative.
+     * Does not use recursion.
+     *
+     * @param sideLength  the length of the triangle to print
+     */
+    public static void printTriangleLoopsOnly(int sideLength) {
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("[]");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
      * Driver method to test printTriangle.
      * @param args  ignored
      */
     public static void main(String[] args) {
+        System.out.println("With recursion:");
         printTriangle(4);
+        System.out.println();
+        System.out.println("Without recursion:");
+        printTriangleLoopsOnly(4);
     }
 }
