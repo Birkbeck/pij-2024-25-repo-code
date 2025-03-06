01497ba1fb05712776b60a3994446d8980951566
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2025-03-06 15:14:02 +0000

    Day 20 solutions

---
+++ src/Day20/main/pij/day20solution/postcode/ManyPostcodesEntry.java
@@ -0,0 +1,17 @@
+package pij.day20solution.postcode;
+
+/**
+ * For Exercise 1.8.
+ */
+public class ManyPostcodesEntry {
+    private String query;
+    private PostcodeResult result;
+
+    @Override
+    public String toString() {
+        return "ManyPostcodesEntry{" +
+                "query='" + query + '\'' +
+                ", result=" + result +
+                '}';
+    }
+}
